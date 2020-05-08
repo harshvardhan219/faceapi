16eb00b2dbe62543927abaf259bcacdee5986f31
harshvardhan219 <44029089+harshvardhan219@users.noreply.github.com>
2020-05-08 12:39:52 +0530

    register fix

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/',(req,res)=>{
 
 app.post('/signin', (req , res) => {signin.handleSignin(req, res, db, bcrypt)})
 
-app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt)})
 
 app.get('/profile/:id', (req , res) =>{profile.handleProfileGet(req, res, db)})
 
