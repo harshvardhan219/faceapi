8008b9eef0833cf7f06b97220b1e912f89615941
harshvardhan219 <44029089+harshvardhan219@users.noreply.github.com>
2020-05-08 13:55:53 +0530

    db chang

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
-const cors   = require('cors');
-const knex   = require('knex');
+const cors = require('cors');
+const knex = require('knex');
+
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-const { Client } = require('pg');
 
-const db = new Client({
+const db = knex({
+	client: 'pg',
+	connection:{
   connectionString: process.env.DATABASE_URL,
   ssl: true,
+}
 });
 
-db.connect();
 
 const app = express();
 
